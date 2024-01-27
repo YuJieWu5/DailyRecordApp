c26939145587d84c62c76b8b4ce2342ecdaddcde
YuJie Wu <wuvivian725@gmail.com>
2024-01-27 13:22:53 -0800

    Add Form widget for diet_recorder.dart

--- lib/diet_recorder.dart
+++ lib/diet_recorder.dart
@@ -29,88 +29,106 @@ class _DietRecorder extends State<DietRecorder>{
 
   void _updateState(){
     setState(() {
-
     });
   }
 
   _addDirectItem(String item){
-    if(!_dietList.contains(item.toLowerCase())){
+    if(!_dietList.contains(item.toLowerCase()) && !item.isEmpty){
       setState(() {
         _dietList.add(item.toLowerCase());
+        _foodDropdown = null;
+        _foodController.clear();
+        _quantityController.clear();
       });
     }
   }
 
   void _onSavePressed(){
     // print("Food: "+ _foodController.text+ " Quantity: "+_quantityController.text);
-
-    _addDirectItem(_foodController.text);
+    if(_foodController.text.isEmpty){
+      _addDirectItem(_foodDropdown.toString());
+      print(_foodDropdown.toString());
+    }
+    else
+      _addDirectItem(_foodController.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-            children: [
-              const Text('Diet Recorder', style: TextStyle(fontSize: 48)),
-              Container(
-                  padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
-                  child: Text('Date: ' + DateTime.now().toString().split(' ')[0], style: TextStyle(fontSize: 20))
-              ),
-              Center(
-                child: _dietList.isNotEmpty
-                    ? DropdownMenu<String>(
-                      enabled: _foodController.text.isEmpty,
-                      requestFocusOnTap: true,
-                      label: const Text('Food'),
-                      onSelected: (newValue) => setState(() {
-                        if(newValue==null || newValue.isEmpty) _foodDropdown = null;
-                        else _foodDropdown = newValue;
-                      }),
-                      dropdownMenuEntries: _dietList.map<DropdownMenuEntry<String>>((String food) {
-                        return DropdownMenuEntry<String>(
-                          value: food,
-                          label: food,
-                        );
-                      }).toList(),
-                    )
-                    : SizedBox.shrink(),
-              ),
-              Center(
-                child: SizedBox(
-                  width: 500.0,
-                  child: TextField(
-                    enabled: _foodDropdown==null,
-                    controller: _foodController,
-                    decoration: const InputDecoration(
-                        labelText: 'Food'
+        appBar: AppBar(
+          title: Center(child: const Text("Diet Recorder", style: TextStyle(fontSize: 30),)),
+        ),
+      body: Form(
+        child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+                    child: Text('Date: ' + DateTime.now().toString().split(' ')[0], style: TextStyle(fontSize: 20))
+                ),
+                Center(
+                  child: _dietList.isNotEmpty
+                      ? DropdownMenu<String>(
+                        enabled: _foodController.text.isEmpty,
+                        requestFocusOnTap: true,
+                        label: const Text('Food'),
+                        onSelected: (newValue) => setState(() {
+                          if(newValue==null || newValue.isEmpty) _foodDropdown = null;
+                          else _foodDropdown = newValue;
+                        }),
+                        dropdownMenuEntries: _dietList.map<DropdownMenuEntry<String>>((String food) {
+                          return DropdownMenuEntry<String>(
+                            value: food,
+                            label: food,
+                          );
+                        }).toList(),
+                      )
+                      : SizedBox.shrink(),
+                ),
+                  SizedBox(
+                    width: 500.0,
+                    child: TextFormField(
+                      enabled: _foodDropdown==null,
+                      controller: _foodController,
+                      decoration: const InputDecoration(
+                          labelText: 'Food'
+                      ),
+                      validator:  (newValue) {
+                        if(newValue == null || newValue.isEmpty) {
+                          return 'Quantity must not be blank.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-              ),
-              Center(
-                child: SizedBox(
-                    width: 500.0,
-                    child: TextField(
-                      controller: _quantityController,
-                      decoration: const InputDecoration(
-                          labelText: 'Quantity'
-                      ),
-                      keyboardType: TextInputType.number,
-                    )
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 20.0),
-                child: ElevatedButton(
-                    onPressed: _onSavePressed,
-                    child: const Text('Save')
-                ),
-              )
-            ],
-          )
-      ),
+                  SizedBox(
+                      width: 500.0,
+                      child: TextFormField(
+                        controller: _quantityController,
+                        decoration: const InputDecoration(
+                            labelText: 'Quantity'
+                        ),
+                        keyboardType: TextInputType.number,
+                        validator: (newValue) {
+                          if(newValue == null || newValue.isEmpty) {
+                            return 'Quantity must not be blank.';
+                          }
+                          return null;
+                        },
+                      )
+                  ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20.0),
+                  child: ElevatedButton(
+                      onPressed: _onSavePressed,
+                      child: const Text('Save')
+                  ),
+                )
+              ],
+            )
+        ),
+      )
     );
   }
 }
