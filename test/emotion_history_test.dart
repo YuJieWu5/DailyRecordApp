7759d8f299535c963f5204ae6cd9ac2226f96cbe
YuJie Wu <wuvivian725@gmail.com>
2024-01-30 21:38:36 -0800

    Add history record testing for emotion page and workout page

---
+++ test/emotion_history_test.dart
@@ -0,0 +1,19 @@
+import 'package:cpsc5250hw/emotion_history.dart';
+import 'package:cpsc5250hw/emotion_record.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main(){
+  testWidgets('Emotion History Testing', (WidgetTester tester) async{
+    List<EmotionRecord> record = [
+      EmotionRecord('😆','happy',DateTime.now()),
+    ];
+
+    await tester.pumpWidget(MaterialApp(home: Material(child: EmotionHistory(record))));
+
+
+    expect(find.text('😆'), findsOneWidget);
+    expect(find.text('happy'), findsOneWidget);
+    expect(find.text(DateTime.now().toString().split(' ')[0]), findsOneWidget);
+  });
+}
---
+++ test/workout_history_test.dart
@@ -0,0 +1,19 @@
+import 'package:cpsc5250hw/workout_history.dart';
+import 'package:cpsc5250hw/workout_record.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main(){
+  testWidgets('Workout History Testing', (WidgetTester tester) async{
+    List<WorkoutRecord> record = [
+      WorkoutRecord('Swimming', 20.0, DateTime.now()),
+    ];
+
+    await tester.pumpWidget(MaterialApp(home: Material(child: WorkoutHistory(record))));
+
+
+    expect(find.text('Swimming'), findsOneWidget);
+    expect(find.text('20.0'), findsOneWidget);
+    expect(find.text(DateTime.now().toString().split(' ')[0]), findsOneWidget);
+  });
+}
